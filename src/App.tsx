d2f5c55b482b790c0f8b042de431f4eab338ba41
ullas-kiran <ullas261@gmail.com>
2024-12-18 19:03:39 +0530

    feat(toolkit):redux-toolkit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import { useDispatch, useSelector } from "react-redux";
-interface RootState {
-  counter: number;
-}
+// import { useDispatch, useSelector } from "react-redux";
+// interface RootState {
+//   counter: number;
+// }
+// const App = () => {
+//   const counter = useSelector((state: RootState) => state.counter);
+//   const dispatch=useDispatch();
+
+//   const increment = () => {
+//     dispatch({type:"INCREMENT"})
+//   };
+
+//   const decrement = () => {
+//     dispatch({type:"DECREMENT"})
+//   };
+
+//   const addValue=()=>{
+//     dispatch({type:"ADDING",payload:10}) //passing value using payload
+//   }
+
+//   return (
+//     <div>
+//       <h1>Counter App</h1>
+//       <h2>{counter}</h2>
+//       <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//       <button onClick={addValue}>Add By 10</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// above old way setup of redux
+
+import { useSelector, useDispatch } from "react-redux";
+import { actions,RootState } from "./store";
+
 const App = () => {
-  const counter = useSelector((state: RootState) => state.counter);
-  const dispatch=useDispatch();
+  const counter = useSelector((state: RootState) => state.counter.counter); // Access nested counter value
+  const dispatch = useDispatch();
 
   const increment = () => {
-    dispatch({type:"INCREMENT"})
+    dispatch(actions.increment()); // No arguments needed
   };
 
   const decrement = () => {
-    dispatch({type:"DECREMENT"})
+    dispatch(actions.decrement()); // No arguments needed
   };
 
-  const addValue=()=>{
-    dispatch({type:"ADDING",payload:10}) //passing value using payload
-  }
+  const addValue = () => {
+    dispatch(actions.addBy(10)); // Pass payload
+  };
 
   return (
     <div>
-      <h1>Counter App</h1>
+      <h1>Counter App With ToolKit</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
@@ -30,3 +64,4 @@ const App = () => {
 };
 
 export default App;
+
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,33 +1,68 @@
-import { createStore } from 'redux';
-const initialState = {
-  counter: 0,
-};
+// import { createStore } from 'redux';
+// const initialState = {
+//   counter: 0,
+// };
 
 
-interface Action<T = number> {
-    type: 'INCREMENT' | 'DECREMENT' | 'ADDING';
-    payload?: T;
-  }
+// interface Action<T = number> {
+//     type: 'INCREMENT' | 'DECREMENT' | 'ADDING';
+//     payload?: T;
+//   }
   
-  type CounterActionTypes = Action<string>;
+//   type CounterActionTypes = Action<string>;
 
-const reducerFn = (state = initialState, action:CounterActionTypes) => {
+// const reducerFn = (state = initialState, action:CounterActionTypes) => {
 
-    // synchronous fn
-    //we should not mutate the original state
-  switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, counter: state.counter + 1 };
-    case 'DECREMENT':
-      return { ...state, counter: state.counter > 0 ? state.counter - 1 : 0 };
-      case 'ADDING':
-        return { ...state, counter: state.counter+ Number(action.payload) };
-    default:
-      return state;
-  }
-};
+//     // synchronous fn
+//     //we should not mutate the original state
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { ...state, counter: state.counter + 1 };
+//     case 'DECREMENT':
+//       return { ...state, counter: state.counter > 0 ? state.counter - 1 : 0 };
+//       case 'ADDING':
+//         return { ...state, counter: state.counter+ Number(action.payload) };
+//     default:
+//       return state;
+//   }
+// };
 
-// Create the Redux store
-const store = createStore(reducerFn);
+// // Create the Redux store
+// const store = createStore(reducerFn);
+
+// export default store;
+
+// above commented the old way doing reudx which require more boilerplatecodes to much complicated while configurinh
+// below new way of redux standard way
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { counter: 0 },
+  reducers: {
+    increment(state) {
+      state.counter++; // No action parameter needed
+    },
+    decrement(state) {
+      state.counter--; // No action parameter needed
+    },
+    addBy(state, action: PayloadAction<number>) {
+      state.counter += action.payload; // Handle payload
+    },
+  },
+});
+
+export const actions = counterSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer, // Add slice under `counter` key
+  },
+});
+
+// Type definitions for RootState and AppDispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
+
